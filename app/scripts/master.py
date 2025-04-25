cce3483d048948dc72a83811787dcd5584562b25
samuel <samueljsanchez24@gmail.com>
2025-04-25 22:01:51 +0200

    correccion imports de app

---
+++ app/scripts/master.py
@@ -0,0 +1,84 @@
+import sys
+import os
+from datetime import datetime, timedelta
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from utils.extraccion_update import extraccion_update
+from utils.limpieza import limpieza_balance, limpieza_demanda, limpieza_generacion, limpieza_fronteras
+from utils.carga import carga_de_datos
+from utils.db_connect import db_connect
+from utils.last_date import get_last_date
+
+
+def actualizacion_general():
+    conn = db_connect()
+
+    tablas = ['balance', 'demanda_evolucion', 'demanda_ire_general', 'estructura_generacion', 'fronteras']
+    last_date = get_last_date(tablas)
+    act_date = datetime.today()
+
+    print(f"Última fecha en la base de datos: {last_date}")
+    print(f"Fecha de actualización: {act_date}")
+    if last_date is None:
+        print("No hay datos en la base de datos.")
+        return
+    if last_date == act_date.date():
+        print("Datos ya actualizados.")
+        return
+
+    start = last_date + timedelta(days=1)
+    end = act_date
+    print(f"Actualizando desde {start} hasta {end}")
+
+    if upgrade_dataframes := extraccion_update(start, end):
+        dfs_limpios = {}
+
+        # Limpieza de los DataFrames simples (uno a uno)
+        for nombre, df in upgrade_dataframes.items():
+            try:
+                if nombre == 'balance':
+                    df_balance_limpio = limpieza_balance(df)
+                elif nombre == 'generacion':
+                    df_generacion_limpio = limpieza_generacion(df)
+                elif nombre == 'fronteras':
+                    df_fronteras_limpio = limpieza_fronteras(df)
+                else:
+                    print(f"Nombre de DataFrame no reconocido: {nombre}")
+                    continue
+            except Exception as e:
+                print(f"Error limpiando el DataFrame {nombre}: {e}")
+                continue
+
+        # Limpieza de los DataFrames de demanda (se procesan juntos)
+        try:
+            df1_demanda = upgrade_dataframes['demanda']
+            df2_demanda = upgrade_dataframes['ire_general']
+            df3_demanda = upgrade_dataframes['ire_industria']
+            df4_demanda = upgrade_dataframes['ire_servicios']
+
+            df_demanda_evolucion_limpio, df_ire_limpio = limpieza_demanda(df1_demanda, df2_demanda, df3_demanda, df4_demanda)
+
+            print("df_demanda_evolucion_limpio y df_ire_limpio generados")
+        except Exception as e:
+            print(f"Error limpiando los datos de demanda: {e}")
+
+        new_data = carga_de_datos(df_balance_limpio, df_generacion_limpio, df_fronteras_limpio, df_demanda_evolucion_limpio, df_ire_limpio)
+        if new_data:
+            print("Datos cargados correctamente.")
+        else:
+            print("Error al cargar los datos.")
+
+    conn.close()
+    print("Actualización completa.")
+
+    return {
+        'balance': df_balance_limpio,
+        'generacion': df_generacion_limpio,
+        'fronteras': df_fronteras_limpio,
+        'demanda_evolucion': df_demanda_evolucion_limpio,
+        'demanda_ire': df_ire_limpio
+    }
+
+# if __name__ == "__main__":
+#     actualizacion_general()
---
+++ app/scripts/utils/extraccion_update.py
@@ -0,0 +1,426 @@
+# %%
+import requests
+import pandas as pd
+import numpy as np
+from pprint import pprint
+from datetime import datetime
+
+# %%
+# Constantes comunes
+URL = "https://apidatos.ree.es" # URL base de la API
+HEADERS = {
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+    "Host": "apidatos.ree.es",
+    "User-Agent": "Mozilla/5.0"
+}
+
+# Diccionario de geo_ids y nombres de regiones
+region_ids = {
+    "8741": "peninsular",
+    "8742": "canarias",
+    "8743": "baleares",
+    "8744": "ceuta",
+    "8745": "melilla",
+    "4": "andalucia",
+    "5": "aragon",
+    "6": "cantabria",
+    "7": "castilla la mancha",
+    "8": "castilla y leon",
+    "9": "cataluña",
+    "10": "pais vasco",
+    "11": "principado de asturias",
+    "13": "comunidad de madrid",
+    "14": "comunidad de navarra",
+    "15": "comunidad de valenciana",
+    "16": "extremadura",
+    "17": "galicia",
+    "20": "la rioja",
+    "21": "region de murcia"
+}
+
+# %%
+filtro_balance = '/es/datos/balance/balance-electrico'
+
+filtro_demanda = '/es/datos/demanda/evolucion'
+filtro_general = '/es/datos/demanda/ire-general'
+filtro_industria = '/es/datos/demanda/ire-industria'
+filtro_servicios = '/es/datos/demanda/ire-servicios'
+
+filtro_generacion = "/es/datos/generacion/estructura-generacion"
+filtro_renovable = "/es/datos/generacion/evolucion-renovable-no-renovable"
+
+# %%
+def Balance_electrico(filtro_balance, URL, HEADERS, start_date, end_date):
+
+    filtro_balance
+
+    endpoint = f"{URL}/{filtro_balance}"
+
+    # Inicializamos un DataFrame vacío para almacenar los datos
+    df_balance = pd.DataFrame()
+
+    TIME_TRUNC = "day"
+
+
+    for geo_id, region_name in region_ids.items():
+        params = {
+            "start_date": start_date.strftime("%Y-%m-%dT%H:%M"),
+            "end_date": end_date.strftime("%Y-%m-%dT%H:%M"),
+            "time_trunc": TIME_TRUNC,
+            "geo_id": geo_id
+        }
+
+        response = requests.get(endpoint, headers=HEADERS, params=params)
+
+        data = response.json()
+        included = data.get('included', [])
+
+        registros = []
+        # Extraemos los datos de balance
+        # para cada grupo de energía
+        # y cada indicador
+        for grupo in included:
+            group_name = grupo['attributes']['title']
+            contenidos = grupo['attributes'].get('content', [])
+
+            for item in contenidos:
+                indicador = item['attributes']['title']
+                valores = item['attributes'].get('values', [])
+                # Extraemos los valores para cada indicador
+                # y los almacenamos en el DataFrame
+                for punto in valores:
+                    registros.append({
+                        'fecha': punto['datetime'],
+                        'valor': punto['value'],
+                        'tipo': indicador,
+                        'energia': group_name,
+                        'region': region_name,
+                    })
+        # Convertimos los registros a un DataFrame
+        # y lo concatenamos al DataFrame principal
+        df_balance_year = pd.DataFrame(registros)
+        df_balance = pd.concat([df_balance, df_balance_year], ignore_index=True)
+
+    return df_balance
+
+
+# %%
+def demanda_evolucion(filtro_demanda, URL, HEADERS, start_date, end_date):
+
+    filtro_demanda
+
+    endpoint = f"{URL}/{filtro_demanda}"
+    # Inicializamos un df vacío para almacenar los datos
+    df_demanda = pd.DataFrame()
+
+    TIME_TRUNC = "day"
+
+    for geo_id, region_name in region_ids.items():
+        params = {
+            "start_date": start_date.strftime("%Y-%m-%dT%H:%M"),
+            "end_date": end_date.strftime("%Y-%m-%dT%H:%M"),
+            "time_trunc": TIME_TRUNC,
+            "geo_id": geo_id
+        }
+
+        response = requests.get(endpoint, headers=HEADERS, params=params)
+
+        data = response.json()
+        included = data.get('included', [])
+
+        registros = []
+
+        for serie in included:
+            indicador = serie['attributes']['title']
+            valores = serie['attributes'].get('values', [])
+            # Extraer los valores de la serie
+            for punto in valores:
+                registros.append({
+                    'fecha': punto['datetime'],
+                    'valor': punto['value'],
+                    'indicador': indicador,
+                    'region': region_name,
+                    })
+
+        df_demanda_year = pd.DataFrame(registros)
+        df_demanda = pd.concat([df_demanda, df_demanda_year], ignore_index=True)
+
+    return df_demanda
+
+# %%
+def demanda_ire_general(filtro_general, URL, HEADERS, start_date, end_date):
+    
+    filtro_general
+
+    endpoint = f"{URL}/{filtro_general}"
+
+    # Inicializamos un DataFrame vacío
+    df_ire_general = pd.DataFrame()
+
+    TIME_TRUNC = "month"
+
+
+    for geo_id, region_name in region_ids.items():
+        params = {
+            "start_date": start_date.strftime("%Y-%m-%dT%H:%M"),
+            "end_date": end_date.strftime("%Y-%m-%dT%H:%M"),
+            "time_trunc": TIME_TRUNC,
+            "geo_id": geo_id
+        }
+
+        response = requests.get(endpoint, headers=HEADERS, params=params)
+
+        if response.status_code != 200:
+            print(f"Datos ya actualizados o no disponibles para región: {region_name}")
+            continue
+
+        data = response.json()
+        included = data.get('included', [])
+
+        registros = []
+
+        for serie in included:
+            indicador = serie['attributes']['title']
+            valores = serie['attributes'].get('values', [])
+            for punto in valores:
+                registros.append({
+                    'fecha': punto['datetime'],
+                    'valor': punto['value'],
+                    'porcentaje': punto.get('percentage'),
+                    'indicador': indicador,
+                    'region': region_name,
+                })
+
+        df_ire_year = pd.DataFrame(registros)
+        df_ire_general = pd.concat([df_ire_general, df_ire_year], ignore_index=True)
+
+    return df_ire_general
+
+# %%
+def demanda_ire_industria(filtro_industria, URL, HEADERS, start_date, end_date):
+
+    filtro_industria
+
+    endpoint = f"{URL}/{filtro_industria}"
+
+    # Inicializamos un DataFrame vacío
+    df_ire_industria = pd.DataFrame()
+
+    TIME_TRUNC = "month"
+
+    for geo_id, region_name in region_ids.items():
+        params = {
+            "start_date": start_date.strftime("%Y-%m-%dT%H:%M"),
+            "end_date": end_date.strftime("%Y-%m-%dT%H:%M"),
+            "time_trunc": TIME_TRUNC,
+            "geo_id": geo_id
+        }
+
+        response = requests.get(endpoint, headers=HEADERS, params=params)
+
+        if response.status_code != 200:
+                print(f"Datos ya actualizados o no disponibles para región: {region_name}")
+                continue
+
+        data = response.json()
+        included = data.get('included', [])
+
+        registros = []
+
+        for serie in included:
+            indicador = serie['attributes']['title']
+            valores = serie['attributes'].get('values', [])
+            for punto in valores:
+                registros.append({
+                    'fecha': punto['datetime'],
+                    'valor': punto['value'],
+                    'porcentaje': punto.get('percentage'),
+                    'indicador': indicador,
+                    'region': region_name,
+                })
+
+        df_ire_industria_year = pd.DataFrame(registros)
+        df_ire_industria = pd.concat([df_ire_industria, df_ire_industria_year], ignore_index=True)
+
+    return df_ire_industria
+
+# %%
+def demanda_ire_servicios(filtro_servicios, URL, HEADERS, start_date, end_date):
+
+    filtro_servicios
+
+    endpoint = f"{URL}/{filtro_servicios}"
+
+    # Inicializamos un DataFrame vacío
+    df_ire_servicios = pd.DataFrame()
+
+    TIME_TRUNC = "month"
+
+    for geo_id, region_name in region_ids.items():
+        params = {
+            "start_date": start_date.strftime("%Y-%m-%dT%H:%M"),
+            "end_date": end_date.strftime("%Y-%m-%dT%H:%M"),
+            "time_trunc": TIME_TRUNC,
+            "geo_id": geo_id
+        }
+
+        response = requests.get(endpoint, headers=HEADERS, params=params)
+
+        if response.status_code != 200:
+            print(f"Datos ya actualizados o no disponibles para región: {region_name}")
+            continue
+
+        data = response.json()
+        included = data.get('included', [])
+
+        registros = []
+
+        for serie in included:
+            indicador = serie['attributes']['title']
+            valores = serie['attributes'].get('values', [])
+            for punto in valores:
+                registros.append({
+                    'fecha': punto['datetime'],
+                    'valor': punto['value'],
+                    'porcentaje': punto.get('percentage'),
+                    'indicador': indicador,
+                    'region': region_name,
+                })
+
+        df_ire_servicios_year = pd.DataFrame(registros)
+        df_ire_servicios = pd.concat([df_ire_servicios, df_ire_servicios_year], ignore_index=True)
+
+    return df_ire_servicios
+
+# %%
+def generacion(filtro_generacion, URL, HEADERS, start_date, end_date):
+
+    filtro_generacion
+
+    endpoint = f"{URL}{filtro_generacion}"
+
+    df_generacion = pd.DataFrame()
+
+    TIME_TRUNC = "day"
+
+    for geo_id, region_name in region_ids.items():
+        params = {
+        "start_date": start_date.strftime("%Y-%m-%dT%H:%M"),
+        "end_date": end_date.strftime("%Y-%m-%dT%H:%M"),
+        "time_trunc": TIME_TRUNC,
+        "geo_id": geo_id
+    }
+
+        response = requests.get(endpoint, headers=HEADERS, params=params)
+
+        if response.status_code == 200:
+            try:
+                data = response.json()
+                included = data.get('included', [])
+
+                if included:
+                    registros = []
+
+                    # Iterar sobre los grupos de datos en 'included'
+                    for grupo in included:
+                        group_name = grupo['attributes'].get('title', 'Desconocido')
+                        group_type = grupo['attributes'].get('type', 'Desconocido')
+                        contenidos = grupo['attributes'].get('values', [])
+
+                        # Iterar sobre los valores de generación
+                        for punto in contenidos:
+                            registros.append({
+                                'fecha': punto.get('datetime', 'Desconocido'),
+                                'valor': punto.get('value', 0),
+                                'porcentaje': punto.get('percentage', 0),
+                                'indicador': group_name,
+                                'region': region_name,
+                                'tipo': group_type,
+                                })
+
+                    df_generacion_year = pd.DataFrame(registros)
+                    df_generacion = pd.concat([df_generacion, df_generacion_year], ignore_index=True)
+
+            except Exception as e:
+                print("Contenido de la respuesta:", response.text)
+
+        else:
+            print(f"Error en la solicitud para {region_name}. Código: {response.status_code}")
+
+    return df_generacion
+# %%
+def fronteras(URL, HEADERS, start_date, end_date):
+    # Lista de países
+    lista_paises = ['francia-frontera', 'portugal-frontera', 'marruecos-frontera', 'andorra-frontera']
+
+    datos_intercambios = []
+
+    for pais in lista_paises:
+        filtro_intercambio = f"/es/datos/intercambios/{pais}"
+        endpoint = f"{URL}{filtro_intercambio}"
+        TIME_TRUNC = "day"
+
+        params = {
+            "start_date": start_date.strftime("%Y-%m-%dT%H:%M"),
+            "end_date": end_date.strftime("%Y-%m-%dT%H:%M"),
+            "time_trunc": TIME_TRUNC,
+        }
+
+        try:
+            response = requests.get(endpoint, headers=HEADERS, params=params)
+            if response.status_code == 200:
+                data = response.json()
+                valores = data["included"][0]["attributes"].get("values", [])
+                if valores:
+                    df = pd.DataFrame(valores)
+                    df["pais"] = pais
+                    datos_intercambios.append(df)
+                else:
+                    print(f"No hay datos disponibles para {pais}.")
+            else:
+                print(f"Error en la solicitud para {pais}. Código: {response.status_code}")
+        except Exception as e:
+            print(f"Error al consultar {pais}: {e}")
+
+    # Solo si hay datos válidos los concatenamos
+    if datos_intercambios:
+        df_fronteras = pd.concat(datos_intercambios, ignore_index=True)
+    else:
+        print("No se obtuvieron datos de fronteras.")
+        df_fronteras = pd.DataFrame()
+
+    return df_fronteras
+
+# %%
+def extraccion_update(start_date, end_date):
+    """
+    Función para extraer datos de la API de REE actualizados.
+    """
+    print('Extrayendo datos actualizados del Balance Eléctrico...')
+    df_balance = Balance_electrico(filtro_balance, URL, HEADERS, start_date, end_date)
+    
+    print('Extrayendo datos actualizados de la Demanda Eléctrica...')
+    df_demanda = demanda_evolucion(filtro_demanda, URL, HEADERS, start_date, end_date)
+    print('Extrayendo datos actualizados de la IRE General...')
+    df_ire_general = demanda_ire_general(filtro_general, URL, HEADERS, start_date, end_date)
+    print('Extrayendo datos actualizados de la IRE Industria...')
+    df_ire_industria = demanda_ire_industria(filtro_industria, URL, HEADERS, start_date, end_date)
+    print('Extrayendo datos actualizados de la IRE Servicios...')
+    df_ire_servicios = demanda_ire_servicios(filtro_servicios, URL, HEADERS, start_date, end_date)
+
+    print('Extrayendo datos actualizados de la Generación Eléctrica...')
+    df_generacion = generacion(filtro_generacion, URL, HEADERS, start_date, end_date)
+
+    print('Extrayendo datos actualizados de los intercambios entre países...')
+    df_fronteras = fronteras(URL, HEADERS, start_date, end_date)
+
+    return {
+        "balance": df_balance,
+        "demanda": df_demanda,
+        "ire_general": df_ire_general,
+        "ire_industria": df_ire_industria,
+        "ire_servicios": df_ire_servicios,
+        "generacion": df_generacion,
+        "fronteras": df_fronteras,
+    }
